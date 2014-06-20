eeeee1bf4020ad6965e59a46961f9e6cee9ca015
77u4 <github@jh0.eu>
2014-06-20 02:41:07 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Uptime
+Show your current uptime on your webpage. Update via bash.
+
+## usage
+````bash
+./uptime.sh -m 'My message!'
+```
+
+````bash
+./uptime.sh -t [unix timestamp when you woke up]
+```
---
+++ client/uptime.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+#uptime bash script
+#Author: Jannis Hutt <hutt@jh0.eu>
+
+#code is enough documentation
+
+time=0 #no comment.
+msg="" #message
+auth="asdf" #password
+gotoFail=0
+success=0
+newMsg=""
+
+sleeptime=2
+
+case "$1" in
+	-m) 
+		msg="$2" ;;
+	-t) 
+		time="$2" ;;
+	*) 
+		gotoFail=1 ;;
+esac
+
+#read msg with spaces
+if ![[ -z "$msg" ]];then
+    msg="${str:4}"
+fi
+
+if [ $(echo "$time" | wc -c) == 0 ] && [ "$msg" == "" ]; then
+	gotoFail=1
+elif [ "$time" != 0 ] && [ $(echo "$time" | wc -c) -lt 11  ]; then
+	gotoFail=2
+fi
+
+if [ "$gotoFail" == "1" ]; then
+	echo "[FAIL] Falsche Parameter"
+elif [ "$gotoFail" == "2" ]; then
+	echo "[FAIL] Die Zeit ist ein Integer mit 10 chars."
+elif [ "$gotoFail" == "3" ]; then
+	echo "[FAIL] Ein unbekannter Fehler ist aufgetreten. Schlagen sie den Dev."
+else
+	if [ "$1" == "-t" ] && [ "$2" != 0 ]; then
+		echo "[INFO] Zeit aktualisieren..."
+		echo "[INFO] Zeit: $2" && sleep $sleeptime
+		curl --get -d auth=$auth -d time=$time http://uptime.jh0.eu/set.php && sleep $sleeptime
+		success=1
+	elif [ "$1" == "-m" ]; then
+	    echo "[INFO] Nachricht aktualisieren..."
+		echo "[INFO] Nachricht: $msg"
+		msg="$(echo "$msg" | sed -e 's/ /\%20/g')"
+		echo "$msg"
+		curl --get -d auth=$auth -d msg="$msg" http://uptime.jh0.eu/set.php && sleep $sleeptime
+		success=1
+	fi
+	if [ "$success" > 0 ]; then
+	    echo "[SUCCESS] Aktualisiert."
+	fi
+fi
